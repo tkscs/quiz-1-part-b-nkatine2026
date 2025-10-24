5992d79c0d970bfc0193e4fee4d52c116aab4ab6
nkatine2026 <nkatine2026@kehillahstudent.org>
2025-10-24 11:36:20 -0700

    done

--- main.py
+++ main.py
@@ -1,5 +1,15 @@
 import turtle
 
 # YOUR CODE HERE
-
+turtle.lt(90)
+turtle.fd(50)
+turtle.right(90)
+for i in range(4):
+    turtle.forward(100)
+    turtle.right(90)
+turtle.fd(50)
+turtle.right(45)
+for i in range(4):
+    turtle.fd((71))
+    turtle.right(90)
 turtle.exitonclick()
